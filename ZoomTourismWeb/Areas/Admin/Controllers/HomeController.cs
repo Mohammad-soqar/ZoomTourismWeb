36c612dd6c860a81e18a79e25b8378694ffdcac5
Mohammad Soqar <mnsoqar1@gmail.com>
2023-10-20 22:49:40 +0300

    Create HomeController.cs

---
+++ ZoomTourismWeb/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZoomTourismWeb.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public IActionResult CallCenterDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult BookingDepartmentDashboard()
+        {
+            return View();
+        }
+    }
+}
