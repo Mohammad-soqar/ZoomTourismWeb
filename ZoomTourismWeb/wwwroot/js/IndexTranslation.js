9796dd310ea8a3a9b3c32ae9f1b6c1411dcdbfed
Mohammad Soqar <mnsoqar1@gmail.com>
2023-11-04 13:26:12 +0300

    Translation files

---
+++ ZoomTourismWeb/wwwroot/js/IndexTranslation.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+   
+    // Load the user's preferred language from localStorage, if available
+    var userLanguage = localStorage.getItem('userLanguage');
+    if (userLanguage) {
+        // Set the language selector to the user's preferred language
+        $('#languageSelector').val(userLanguage);
+        // Load the user's preferred language
+        loadLanguage(userLanguage);
+    } else {
+        // If no preference is set, load the default language (English)
+        loadLanguage('en');
+    }
+
+    // Handle language selection change
+    $('#languageSelector').change(function () {
+        var selectedLanguage = $(this).val();
+        // Store the user's language preference in localStorage
+        localStorage.setItem('userLanguage', selectedLanguage);
+        loadLanguage(selectedLanguage);
+    });
+});
+
+function loadLanguage(language) {
+    // Determine the current page from the JavaScript variable
+    var currentPage = window.currentPage;
+
+    $.getJSON('/Translation/languages.json', function (data) {
+        var translations = data[language][currentPage];
+        $('.translate').each(function () {
+            var key = $(this).data('translate-key');
+            $(this).text(translations[key]);
+        });
+    }).done(function () {
+        console.log('JSON file loading completed.');
+    }).fail(function (jqxhr, textStatus, error) {
+        console.error('Error loading JSON file:', textStatus, error);
+    });
+}
