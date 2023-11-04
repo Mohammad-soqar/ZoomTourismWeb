8874a4526420250ed1a59a9a6cca32ade9b120ff
Mohammad Soqar <mnsoqar1@gmail.com>
2023-11-04 13:23:11 +0300

    Create Employee.cs

---
+++ ZoomTourismWeb/Areas/Admin/Controllers/Employee.cs
@@ -0,0 +1,34 @@
+﻿using CodyleOffical.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ZoomTourism.DataAccess.Repository.IRepository;
+using ZoomTourism.Models;
+
+namespace ZoomTourismWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_CodyleSupport)]
+
+    public class Employee : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _HostEnvironment;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public Employee(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment,
+            UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _HostEnvironment = hostEnvironment;
+            _userManager = userManager;
+        }
+        public IActionResult Employees()
+        {
+
+            var users = _userManager.Users.ToList(); 
+            return View(users);
+        }
+    }
+}
